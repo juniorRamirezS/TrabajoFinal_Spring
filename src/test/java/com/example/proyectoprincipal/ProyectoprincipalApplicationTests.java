d6795af3c7383fa0ff60292286421fc7bbfb91e2
Junior <Junior@DESKTOP-DL95D65>
2023-01-18 06:23:28 -0500

    Proyecto cargado

---
+++ src/test/java/com/example/proyectoprincipal/ProyectoprincipalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoprincipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoprincipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
